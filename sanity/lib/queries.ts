c4278dad28543c722e9d4e141805f5988e9558e8
Askari <askarir64@gmail.com>
2025-08-19 18:18:19 +0530

    updated posts query to include imgs

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -9,6 +9,7 @@ const postFields = /* groq */ `
   "slug": slug.current,
   excerpt,
   coverImage,
+  images[],  
   "date": coalesce(date, _updatedAt),
   "author": author->{"name": coalesce(name, "Anonymous"), picture},
 `;
